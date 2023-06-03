64f03e2350dc893d593e0f7af29da8d9b93a4d06
Xavier Berthiaume <xavier.berthiaume@gmail.com>
2023-06-03 07:21:32 -0400

    Adding line chart

--- main.cpp
+++ main.cpp
@@ -16,45 +16,170 @@
 
 QT_CHARTS_USE_NAMESPACE
 
+QChartView *createBarChart();
+QChartView *createStackedBarChart();
+QChartView *createLineChart();
+
 int main(int argc, char *argv[])
 {
+    QApplication a(argc, argv);
+
+    QChartView *chartView = createStackedBarChart();
+
+    MainWindow w;
+
+    // Make the created chart a widget that will be displayed
+    w.setCentralWidget(chartView);
+    w.resize(500, 400);
+
+    w.show();
+
+    return a.exec();
+}
+
+QChartView *createBarChart() {
+    // Assign names to the set of bars
     QBarSet *set0 = new QBarSet("Altuve");
     QBarSet *set1 = new QBarSet("Martinez");
     QBarSet *set2 = new QBarSet("Segura");
     QBarSet *set3 = new QBarSet("Simmons");
     QBarSet *set4 = new QBarSet("Trout");
 
+    // Assign values for each bar to be displayed
     *set0 << 283 << 341 << 313 << 338 << 346 << 335;
     *set1 << 250 << 315 << 282 << 307 << 303 << 330;
     *set2 << 294 << 246 << 257 << 319 << 300 << 325;
     *set3 << 248 << 244 << 265 << 281 << 278 << 313;
     *set4 << 323 << 287 << 299 << 315 << 306 << 313;
 
+    // Add all sets of data to the chart
     QBarSeries *series = new QBarSeries();
+
+    // Stack the bar chart
     series->append(set0);
     series->append(set1);
     series->append(set2);
     series->append(set3);
     series->append(set4);
 
+    // Define the bar chart to display
     QChart *chart = new QChart();
+    // Add the data to the chart
     chart->addSeries(series);
+
+    // Set the title name
     chart->setTitle("Batting average by year");
+
+    // Set the opening animation
+    // Options are NoAnimation, GridAxisAnimation, SeriesAnimations
     chart->setAnimationOptions(QChart::AllAnimations);
 
+    // Create the category names
     QStringList categories;
     categories << "2013" << "2014" << "2015" << "2016" << "2017" << "2018";
-    QBarCategoryAxis *axis = QBarCategoryAxis();
+
+    // Add the category names to an axis
+    QBarCategoryAxis *axis = new QBarCategoryAxis();
     axis->append(categories);
-
     chart->createDefaultAxes();
-    chart->setAxis(axis, series);
+
+    // For a Bar Chart
+    // chart->setAxisX(axis, series); DEPRECATED FUNCTION, USE BELOW
+    chart->addAxis(axis, Qt::AlignBottom);
+
+    // Define where the legend is displayed
     chart->legend()->setVisible(true);
-    chart->legned()->setAlignment(Qt::AlignLeft);
+    chart->legend()->setAlignment(Qt::AlignBottom);
 
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    // Display the chart
+    QChartView *chartView = new QChartView(chart);
+    chartView ->setRenderHint(QPainter::Antialiasing);
 
-    return a.exec();
+    // Change the display color palette
+    QPalette palette = qApp->palette();
+    palette.setColor(QPalette::Window, QRgb(0x111111));
+    palette.setColor(QPalette::WindowText, QRgb(0x404040));
+    qApp->setPalette(palette);
+
+    return chartView;
+}
+
+QChartView *createStackedBarChart() {
+    // Assign names to the set of bars
+    QBarSet *set0 = new QBarSet("Altuve");
+    QBarSet *set1 = new QBarSet("Martinez");
+    QBarSet *set2 = new QBarSet("Segura");
+    QBarSet *set3 = new QBarSet("Simmons");
+    QBarSet *set4 = new QBarSet("Trout");
+
+    // Assign values for each bar to be displayed
+    *set0 << 283 << 341 << 313 << 338 << 346 << 335;
+    *set1 << 250 << 315 << 282 << 307 << 303 << 330;
+    *set2 << 294 << 246 << 257 << 319 << 300 << 325;
+    *set3 << 248 << 244 << 265 << 281 << 278 << 313;
+    *set4 << 323 << 287 << 299 << 315 << 306 << 313;
+
+    // Add all sets of data to the chart
+    QHorizontalStackedBarSeries *series = new QHorizontalStackedBarSeries();
+
+    // Stack the bar chart
+    series->append(set0);
+    series->append(set1);
+    series->append(set2);
+    series->append(set3);
+    series->append(set4);
+
+    // Define the bar chart to display
+    QChart *chart = new QChart();
+    // Add the data to the chart
+    chart->addSeries(series);
+
+    // Set the title name
+    chart->setTitle("Batting average by year");
+
+    // Set the opening animation
+    // Options are NoAnimation, GridAxisAnimation, SeriesAnimations
+    chart->setAnimationOptions(QChart::AllAnimations);
+
+    // Create the category names
+    QStringList categories;
+    categories << "2013" << "2014" << "2015" << "2016" << "2017" << "2018";
+
+    // Add the category names to an axis
+    QBarCategoryAxis *axis = new QBarCategoryAxis();
+    axis->append(categories);
+    chart->createDefaultAxes();
+
+    // For a Bar Chart
+    // chart->setAxisX(axis, series); DEPRECATED FUNCTION, USE BELOW
+    chart->addAxis(axis, Qt::AlignLeft);
+
+    // Define where the legend is displayed
+    chart->legend()->setVisible(true);
+    chart->legend()->setAlignment(Qt::AlignBottom);
+
+    // Display the chart
+    QChartView *chartView = new QChartView(chart);
+    chartView ->setRenderHint(QPainter::Antialiasing);
+
+    // Change the display color palette
+    QPalette palette = qApp->palette();
+    palette.setColor(QPalette::Window, QRgb(0xffffff));
+    palette.setColor(QPalette::WindowText, QRgb(0x404040));
+    qApp->setPalette(palette);
+
+    return chartView;
+}
+
+QChartView *createLineChart() {
+    QLineSeries *series = new QLineSeries();
+    series->append(0, 16);
+    series->append(1, 25);
+    series->append(2, 24);
+    series->append(3, 19);
+    series->append(4, 33);
+    series->append(5, 25);
+    series->append(6, 34);
+
+    QChart *chart = new QChart();
 }
